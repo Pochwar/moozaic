68998b6dde30d38f0c1a9ab6dff5aaf98bdfa313
Benoît Ripoche <ripoche.b@gmail.com>
2018-12-17 17:56:49 +0100

    set output mozaic by project

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,4 @@
 var mozaic = document.querySelector('#mozaic')
 setInterval(function() {
-  console.log('refresh img')
-  mozaic.src="img/mozaic.png?random=" + new Date().getTime()
+  mozaic.src= img + "?random=" + new Date().getTime()
 }, 120000)
